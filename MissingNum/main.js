479c76db01689aa238ee5a063150ddb8cafd5e2d
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-11-12 18:08:11 -0800

    check for missing num in array

---
+++ MissingNum/main.js
@@ -0,0 +1,15 @@
+// Given an array nums containing 'n' distinct numbers in the range [0, n],
+// return the only number in the range that is missing from the array.
+
+
+
+function missingNumber(nums){
+    const expectedSum = (nums.length * (nums.length + 1)) / 2;
+    const actualSum = nums.reduce((sum, num) => sum + num, 0);
+    
+    return expectedSum - actualSum;
+}
+
+
+console.log(missingNumber([3, 0, 1])) // 2
+console.log(missingNumber([0, 4, 1, 2, 3, 6])) // 5
