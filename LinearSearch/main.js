72aa0e2f99f5e9df2b3bc7d9cef9370577588ee1
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-11-13 18:10:02 -0800

    linear search on list, return index

---
+++ LinearSearch/main.js
@@ -0,0 +1,16 @@
+// Implement a function that performs a linear search on a list to find-
+// the index of a specified item. (-1 if not found) & log the result.
+
+
+
+function linearSearch(list, item){
+    let index = -1;
+    list.forEach((listItem, i) => {
+        if(listItem === item) {
+            index = i;
+        }
+    });
+    return index;
+}
+
+console.log(linearSearch([2,6,7,90,103], 90)) // 3
