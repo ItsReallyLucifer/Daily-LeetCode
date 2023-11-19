ad0031f95a6f90c4a4e2f1f4b774e62e49957797
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-11-18 16:33:32 -0800

    Return indices of sum to target num

---
+++ SumIndex/main.js
@@ -0,0 +1,22 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order./
+//Example 1:
+
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+function twoSum (arr, target) {
+    for(let i = 0; i < arr.length; i++){
+        for(let k = 0; k < arr.length; k++){
+            if(arr[i] + arr[k] === target && i !== k){
+                return [i,k]
+            }
+        }
+    }
+}
+
+console.log(twoSum([1,2,3,4,5], 9)) // 3,4
+console.log(twoSum([1,2,3,4,5], 4)) // 0,2
