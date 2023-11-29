ef83491d5bfa208ef3e3858b8f7dad35fc26d656
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-11-29 08:37:48 -0800

    Determines if array/object contains elements

---
+++ ReturnEmpty/main.js
@@ -0,0 +1,39 @@
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+// Example 1:
+// Input: obj = {"x": 5, "y": 42} || false
+
+// Example 2:
+// Input: obj = {} || true
+
+
+
+function isEmpty (obj) {
+    if(Array.isArray(obj)) {
+        return obj.length === 0;
+    }
+
+    if(typeof obj === 'object' && obj !== null) {
+        return Object.keys(obj).length === 0;
+    }
+
+    return true
+};
+
+const obj = {};
+const arr = [];
+
+console.log(isEmpty(obj)); //  true
+console.log(isEmpty(arr)); //  true
+
+const nonEmptyObj = { key: 'value' };
+const nonEmptyArr = [1, 2, 3];
+
+console.log(isEmpty(nonEmptyObj)); // false
+console.log(isEmpty(nonEmptyArr)); // false
+
+
