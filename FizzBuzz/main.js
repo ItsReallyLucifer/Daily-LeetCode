7c9346fa0fdd4fe4d481fee9c94125a7d4e2f217
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-12-20 20:43:38 -0800

    Loop through integers, return divisible condition

---
+++ FizzBuzz/main.js
@@ -0,0 +1,26 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+
+
+function fizzBuzz(n){
+    for(i = 1; i <= n; i++){
+     if(i % 3 === 0 && i % 5 === 0){
+        console.log('fizzbuzz')
+     }else if(i % 3 === 0){
+        console.log('fizz')
+     }else if(i % 5 === 0){
+        console.log('buzz')
+     }else{
+        console.log(i)
+     }
+    }
+}
+
+fizzBuzz(5) // 1,2,fizz,4,buzz
+fizzBuzz(9) // 1,2,fizz,4,buzz,fizz,7,8,fizz
+fizzBuzz(15) // 1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz
