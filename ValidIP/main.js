78c3ffe6e32a8a6f304d197f376f6c45cafdab1e
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-12-23 17:22:31 -0800

    Transform IP address char sequence

---
+++ ValidIP/main.js
@@ -0,0 +1,21 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+// Example
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+
+function defangIPaddr(address) {
+    let defanged = "";
+    for (let char of address) {
+        if (char === ".") {
+            defanged += "[.]";
+        } else {
+            defanged += char;
+        }
+    }
+    return defanged;
+}
+
+
+console.log(defangIPaddr("1.1.1.1")); // "1[.]1[.]1[.]1"
