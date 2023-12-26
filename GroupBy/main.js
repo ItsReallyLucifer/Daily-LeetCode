0027667591259af4e437bb45e198148117ce048f
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-12-25 17:39:33 -0800

    Group array items based on provided function

---
+++ GroupBy/main.js
@@ -0,0 +1,26 @@
+// Write a program that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
+// A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array with that key.
+// The provided callback fn will accept an item in the array and return a string key.
+
+// The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
+// Please solve it without lodash's _.groupBy function.
+
+
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, item) => {
+        const key = fn(item);
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(item);
+        return acc;
+    }, {});
+};
+
+const arr = [1, 2, 3, 4, 5];
+const grouped = arr.groupBy((item) => {
+    return item % 2 === 0 ? 'even' : 'odd';
+});
+
+console.log(grouped);
+// { odd: [1, 3, 5], even: [2, 4] }
