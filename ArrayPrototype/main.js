cdf2f02926f4f9c85db577d8db5b8aefb576e874
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-12-28 14:19:57 -0800

    Enhance array prototype to retrieve last element

---
+++ ArrayPrototype/main.js
@@ -0,0 +1,17 @@
+// Write a program that enhances all arrays such that you can call the array.last() method on any array 
+// and it will return the last element. If there are no elements in the array, it should return -1.
+// You may assume the array is the output of JSON.parse.
+
+
+
+Array.prototype.last = function() {
+    if (this.length === 0) {
+        return -1;
+    } else {
+        return this[this.length - 1];
+    }
+};
+
+let nums = [null, {}, 3];
+nums = JSON.parse(JSON.stringify(nums)); 
+console.log(nums.last()); // 3
