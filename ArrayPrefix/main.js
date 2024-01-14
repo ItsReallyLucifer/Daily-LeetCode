c5c4edc3499f87d549b732f0e9f32423a3929a85
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-01-13 19:08:37 -0800

    Construct array based on XOR conditions

---
+++ ArrayPrefix/main.js
@@ -0,0 +1,20 @@
+// You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+// pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+// Note that ^ denotes the bitwise-xor operation.
+// It can be proven that the answer is unique.
+
+
+
+function findArray(pref) {
+    const n = pref.length;
+    let arr = [pref[0]];
+  
+    for (let i = 1; i < n; i++) {
+      arr.push(pref[i - 1] ^ pref[i]);
+    }
+  
+    return arr;
+  }
+
+const prefArray = [5, 2, 0, 3, 1];
+console.log(findArray(prefArray)); // [5,7,2,3,2]
