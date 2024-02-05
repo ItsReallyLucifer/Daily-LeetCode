458d30df7121a8fc644708d7bdbe238665c9e9b9
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-02-04 20:02:04 -0800

    Counts good pairs in given array of nums

---
+++ GoodPairs/main.ts
@@ -0,0 +1,20 @@
+// Given an array of integers nums, return the number of good pairs.
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+
+function numIdenticalPairs(nums: number[]): number {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+const numArr: number[] = [1, 2, 3, 1, 1, 3];
+console.log(numIdenticalPairs(numArr)); // 4
