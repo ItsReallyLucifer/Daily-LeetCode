d6881e654292da2b98a0e9ffdb9c56f23a6108df
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-02-22 09:02:52 -0800

    Generates values from nested array

---
+++ NestedArray/main.ts
@@ -0,0 +1,25 @@
+// Given a multi-dimensional array of integers, return a generator object which yields integers in the same order as inorder traversal.
+// A multi-dimensional array is a recursive data structure that contains both integers and other multi-dimensional arrays.
+// inorder traversal iterates over each array from left to right, yielding any integers it encounters or applying inorder traversal to any arrays it encounters.
+
+
+
+
+function* inorderTraversal(arr: any[]): Generator<any, void, unknown> {
+    function* traverse(element: any): Generator<any, void, unknown> {
+        if (Array.isArray(element)) {
+            for (const subElement of element) {
+                yield* traverse(subElement);
+            }
+        } else {
+            yield element;
+        }
+    }
+
+    yield* traverse(arr);
+}
+
+const arr1: any[] = [[[6]], [1, 3], []];
+const generator1: Generator<any, void, unknown> = inorderTraversal(arr1);
+
+console.log(generator1.next().value); // 6
