cc5ba857ce45e968f5bba59760431f5cd7843d27
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-02-28 14:12:19 -0800

    Identify first repeated letter in str, return character

---
+++ RepeatedChar/main.ts
@@ -0,0 +1,18 @@
+// Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+// A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+// s will contain at least one letter that appears twice.
+
+
+function repeatedCharacter(s: string): string | undefined {
+    const seenChars: Set<string> = new Set();
+
+    for (let i = 0; i < s.length; i++) {
+        if (seenChars.has(s[i])) {
+            return s[i];
+        } else {
+            seenChars.add(s[i]);
+        }
+    }
+}
+
+console.log(repeatedCharacter('abccbaacz'));  // c
