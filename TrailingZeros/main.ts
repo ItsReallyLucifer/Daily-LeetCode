e491d61f508fe17c744ae253a228a8da1da0552e
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-03-15 10:13:43 -0700

    remove trailing zeros from string of nums

---
+++ TrailingZeros/main.ts
@@ -0,0 +1,19 @@
+// Given a positive integer num represented as a string, 
+//return the integer num without trailing zeros as a string.
+
+
+
+function removeTrailingZeros(num: string): string {
+    let endIndex: number = num.length - 1;
+    while (endIndex >= 0 && num[endIndex] === '0') {
+        endIndex--;
+    }
+
+    if (endIndex === num.length - 1) {
+        return num;
+    }
+    
+    return num.slice(0, endIndex + 1);
+}
+
+console.log(removeTrailingZeros('439000')); // 439
