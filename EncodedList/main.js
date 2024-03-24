ca7dddc0cc6ae2bb2c68100a789d69ddfd9e9cb2
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-03-24 09:36:13 -0700

    Decompresses run-length encoded list into array

---
+++ EncodedList/main.js
@@ -0,0 +1,21 @@
+// We are given a list nums of integers representing a list compressed with run-length encoding.
+// Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+// Return the decompressed list.
+
+
+function decompressRLElist(nums) {
+    const result = [];
+    
+    for (let i = 0; i < nums.length; i += 2) {
+        const freq = nums[i];
+        const val = nums[i + 1];
+        for (let j = 0; j < freq; j++) {
+            result.push(val);
+        }
+    }
+    
+    return result;
+}
+
+const nums = [1, 2, 3, 4];
+console.log(decompressRLElist(nums)); // [2, 4, 4, 4]
