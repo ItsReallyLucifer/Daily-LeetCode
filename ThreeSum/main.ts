6bcf982e3f0c2b77ae19f2e46b27db9753e80678
ItsReallyLucifer <itsreallylucifer@gmail.com>
2024-04-08 09:15:26 -0700

    Iterates to find unique triplets summing to zero

---
+++ ThreeSum/main.ts
@@ -0,0 +1,42 @@
+// Given an integer array nums, return all the triplets 
+// [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
+
+
+
+
+function threeSum(nums: number[]): number[][] {
+    nums.sort((a, b) => a - b); 
+
+    const result: number[][] = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+            let left = i + 1;
+            let right = nums.length - 1;
+            const target = 0 - nums[i];
+
+            while (left < right) {
+                const sum = nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([nums[i], nums[left], nums[right]]);
+                    while (left < right && nums[left] === nums[left + 1]) left++; 
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+const nums: number[] = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums)); // [[-1,-1,2],[-1,0,1]]
